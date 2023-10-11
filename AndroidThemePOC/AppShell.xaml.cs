6ac2f6d9fd3dda8559afcd4b9f108bcc85237eb4
Sweekriti Satpathy <swsat@microsoft.com>
2023-10-11 10:35:29 -0700

    first commit

---
+++ AndroidThemePOC/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AndroidThemePOC;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
---
+++ AndroidThemePOC/Platforms/Android/MainActivity.cs
@@ -0,0 +1,14 @@
+﻿using Android.App;
+using Android.Content.PM;
+
+namespace AndroidThemePOC;
+
+[Activity(
+    // Theme = "@style/Maui.SplashTheme", // From the blank MAUI app
+    Theme = "@style/Theme.SplashScreen", // Message (Java.Lang.Throwable): "The style on this component requires your app theme to be Theme.MaterialComponents (or a descendant)."
+    MainLauncher = true,
+    ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+public class MainActivity : MauiAppCompatActivity
+{
+}
+
